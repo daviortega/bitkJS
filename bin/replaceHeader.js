aae55db44dd62a644cbe7fd83e09f335fe1e01e6
Davi Ortega <daviortega@gmail.com>
2017-03-03 10:18:56 -0800

    changing directory name

--- bin/replaceHeader.js
+++ bin/replaceHeader.js
@@ -37,7 +37,7 @@ let	readerStream = fs.createReadStream(path.resolve(args.input)),
 	writerStream = fs.createWriteStream(path.resolve(args.out))
 
 let Transform = require('stream').Transform,
-	keys = require(path.resolve(args.keys))	
+	keys = require(path.resolve(args.keys))
 
 let transform = new Transform()
 
