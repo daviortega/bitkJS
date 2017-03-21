3975d9db917237e190d1fd0cad02f9736cce3b50
Davi Ortega <ortegad@caltech.edu>
2017-03-21 11:37:45 -0700

    indicating that tests are needed

---
+++ src/ParseBlastResults.tests.js
@@ -0,0 +1,9 @@
+/* eslint-disable global-require */
+'use strict'
+
+let expect = require('chai').expect,
+	ParseBlastResults = require('../src/ParseBlastResults.js')
+
+describe('ParseBlastResults unit test', function() {
+	it('all tests needed')
+})
